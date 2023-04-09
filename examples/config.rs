20fa0a06ba3ef0ec223b45a1bfc097f9c8dea797
WmW <zhouzheng0646@qq.com>
2023-04-10 01:39:21 +0800

    init

---
+++ examples/config.rs
@@ -0,0 +1,8 @@
+use anyhow::Result;
+use xdiff::DiffConfig;
+
+fn main() {
+    let content = include_str!("../fixtures/test.yml");
+    let config = DiffConfig::from_yaml(content);
+    println!("【 config 】==> {:#?}", config);
+}
